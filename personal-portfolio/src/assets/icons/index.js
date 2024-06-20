a84d53e67baaebb9ecbd3912d6916713b5f61206
Shikhararora19 <shikhar3@ualberta.ca>
2024-06-20 18:21:37 +0530

    Completed the about page

--- personal-portfolio/src/assets/icons/index.js
+++ personal-portfolio/src/assets/icons/index.js
@@ -26,6 +26,9 @@ import arrow from './arrow.svg'
 import contact from './contact.svg'
 import soundon from './soundon.png'
 import soundoff from './soundoff.png'
+import java from './java.svg'
+import python from './python.svg'
+import sql from './sql.svg'
 
 export {
     css,
@@ -55,5 +58,8 @@ export {
     arrow,
     contact,
     soundon,
-    soundoff
+    soundoff,
+    java,
+    python,
+    sql
 }
