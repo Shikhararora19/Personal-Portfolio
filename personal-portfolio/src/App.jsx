a8f358df70078f302aa827c234709c1701f3a746
Shikhararora19 <shikhar3@ualberta.ca>
2024-06-21 12:35:08 +0530

    Update App.jsx

--- personal-portfolio/src/App.jsx
+++ personal-portfolio/src/App.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Home,About,Projects,Contact } from './Pages'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
 
 const App = () => {
+  useEffect(() => {
+    document.title = 'Personal Portfolio'
+  
+  })
   return (
     <main className='bg-slate-300/20 h-full'>
         <Router>
