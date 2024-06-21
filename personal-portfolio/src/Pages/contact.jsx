6cfd5571cefebe4b6ddee761e98585632b2782d6
Shikhararora19 <shikhar3@ualberta.ca>
2024-06-21 12:17:54 +0530

    Fixed some page issues

--- personal-portfolio/src/Pages/contact.jsx
+++ personal-portfolio/src/Pages/contact.jsx
@@ -54,7 +54,7 @@ const contact = () => {
 
   };
   return (
-    <section className='relative flex lg:flex-row flex-col max-container'>
+    <section className='relative flex lg:flex-row flex-col max-container h-[100vh]'>
       {alert.show && <Alert {...alert}/>}
       <div className='flex-1 min-w-[50%] flex flex-col'>
         <h1 className='head-text'>Get in Touch</h1>
